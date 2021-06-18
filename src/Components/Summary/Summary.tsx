61adaa9927cebc4ecc604a8afa5d129f0d081d27
Junihor Moran <jumoran@idcontractors.net>
2021-06-18 18:07:01 -0500

    Add summary section

--- src/Components/Summary/Summary.tsx
+++ src/Components/Summary/Summary.tsx
@@ -5,6 +5,17 @@ function Summary() {
   return (
     <article className="summary">
       <SectionTitle name="Summary" />
+      <div className="summary__content">
+        <p>I am a Sr Web UI Developer with more than 10 years of experience developing 
+          in platforms using JavaScript, HTML and CSS. The last 3 years of experience he 
+          has used React JS.</p>
+        <p>I work with agile methodologies, has experience leading teams and has a great 
+          ability to communicate and understand the client’s requirements.</p>
+        <p>I am an easygoing person, result-oriented professional focused on the quality 
+          to find the best solution for the client, I have the ability to easily adapts 
+          to change and commits to the projects that I am working on.</p>
+      </div>
+      <button className="summary__viewmore">View more</button>
     </ article>
   );
 }
